6a3901468767ff78c1140ee372ddf756b7a525ca
Lachartre2020 <s.lachartre@hotmail.fr>
2021-06-25 09:25:32 +0200

    Initialisation projet

---
+++ src/test/java/com/stepupacademy/checkpointspring/CheckpointspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepupacademy.checkpointspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckpointspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
